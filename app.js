0d0a7b72ef3a70b0a24a015eb1ef19be3376f097
coderatif <=>
2024-02-11 17:36:31 +0530

    added nodemailer

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
+
 const app = express();
 require('dotenv').config();
 // parse application/x-www-form-urlencoded
@@ -32,9 +34,37 @@ app.get('/', (req, res) => {
   res.send('Resume Backend');
 });
 
-app.post('/customer', (req, res) => {
+app.post('/customer', async (req, res) => {
+  const { email, subject, message, name } = req.body;
   console.log(req.body);
   const newContact = new Customer(req.body);
+
+  // send mail
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+      user: process.env.MAIL,
+      pass: process.env.MAIL_PWD,
+    },
+  });
+
+  // async..await is not allowed in global scope, must use a wrapper
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: email, // sender address
+    to: process.env.MAIL, // list of receivers
+    subject: subject, // Subject line
+    html: `
+    <div>
+    <h1>Sender Name ${name} </h1>
+    <b>${message}</b>
+    </div>
+   `, // html body
+  });
+  console.log('info===>', info);
+
   newContact.save(function (err) {
     if (err) return handleError(err);
     res.status(200).send('Customer saved successfully');
